976efd78753fa7071a768d4cfdbcbee37e860d27
Kenneth Yang <kjy5@uw.edu>
2026-05-03 12:03:00 -0700

    Draft action server

--- src/stretch_pose_server/stretch_pose_server/main.py
+++ src/stretch_pose_server/stretch_pose_server/main.py
@@ -1,14 +1,27 @@
+from geometry_msgs.msg import TransformStamped, PointStamped
 from hello_helpers.hello_misc import HelloNode
+from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.duration import Duration
 
 # noinspection PyUnresolvedReferences
 from stretch_pose_interface.srv import GetPose
+
+# noinspection PyUnresolvedReferences
+from stretch_pose_interface.action import SetPose
 
 # Constants.
 END_FRAME = "link_grasp_center"
 WORLD_FRAME = "odom"
 ROBOT_FRAME = "base_link"
 ARUCO_FRAME = "column_4"
+VALID_FRAMES = {WORLD_FRAME, ROBOT_FRAME, ARUCO_FRAME}
+
+HOME_X = 0
+HOME_Y = -0.5
+HOME_z = 0.7
+HOME_ARM = 0.1
+HOME_LIFT = 0.6
 
 
 class PoseServer(HelloNode):
@@ -26,6 +39,13 @@ class PoseServer(HelloNode):
         # Define GetPose service.
         _get_pose_service = self.create_service(
             GetPose, "get_pose", self._get_pose_callback, callback_group=callback_group
+        )
+
+        # Define SetPose action.
+        _set_pose_service = ActionServer(
+            self, SetPose, 'set_pose', execute_callback=self._set_pose_execute,
+            goal_callback=lambda goal: GoalResponse.ACCEPT, cancel_callback=CancelResponse.ACCEPT,
+            callback_group=callback_group
         )
 
         # Ready.
@@ -59,6 +79,89 @@ class PoseServer(HelloNode):
             response.message = str(e)
 
         return response
+
+    def _set_pose_execute(self, goal_handle):
+        # Build result and feedback objects.
+        result = SetPose.Result()
+        feedback = SetPose.Feedback()
+
+        # Extract target.
+        target: TransformStamped = goal_handle.request.target_pose
+        ref_frame: str = target.header.frame_id
+
+        self.get_logger().info(f"Going to target {target}...")
+
+        # Validate the frame
+        if ref_frame not in VALID_FRAMES:
+            result.success = False
+            result.message = f"Unknown frame: {ref_frame}."
+            goal_handle.abort()
+            return result
+
+        # Convert target position into robot frame.
+        feedback.status = "Looking up target pose in robot frame."
+        goal_handle.publish_feedback(feedback)
+
+        try:
+            target_point = PointStamped()
+            target_point.header.frame_id = ref_frame
+            target_point.header.stamp = self.get_clock().now().to_msg()
+            target_point.point.x = target.transform.translation.x
+            target_point.point.y = target.transform.translation.y
+            target_point.point.z = target.transform.translation.z
+
+            target_in_robot_frame = self.tf2_buffer.transform(target_point, ROBOT_FRAME, timeout=Duration(seconds=1))
+        except Exception as e:
+            feedback.success = False
+            feedback.message = f"Unable to get target pose: {e}."
+            goal_handle.abort()
+            return result
+
+        # Compute joint command.
+        feedback.status = "Computing joint values."
+        goal_handle.publish_feedback(feedback)
+
+        target_x = target_in_robot_frame.point.x
+        target_y = target_in_robot_frame.point.y
+        target_z = target_in_robot_frame.point.z
+
+        joints = self._transform_to_joints(target_x, target_y, target_z)
+
+        self.get_logger().info(
+            f'Joint commands → '
+            f'translate_mobile_base={joints["translate_mobile_base"]:.3f} '
+            f'joint_arm={joints["joint_arm"]:.3f} '
+            f'joint_lift={joints["joint_lift"]:.3f}'
+        )
+
+        # Execute joint command.
+        feedback.status = "Moving to pose..."
+        goal_handle.publish_feedback(feedback)
+
+        try:
+            self.move_to_pose(joints, blocking=True)
+
+            feedback.status = "Done!"
+            goal_handle.publish_feedback(feedback)
+            goal_handle.succeed()
+            result.success = True
+            result.message = "OK"
+        except Exception as e:
+            result.success = False
+            result.message = str(e)
+            goal_handle.abort()
+
+        return result
+
+    @staticmethod
+    def _transform_to_joints(transform_x, transform_y, transform_z):
+        arm = HOME_ARM + (-(transform_y - HOME_Y))
+        lift = transform_z - 0.1
+        return {
+            "translate_mobile_base": transform_x,
+            "joint_arm": arm,
+            "joint_lift": lift,
+        }
 
 
 def main():
