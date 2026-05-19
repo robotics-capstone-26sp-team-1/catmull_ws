ac0af1af199eab6983c120029a849d7b2843f45c
Kenneth Yang <kjy5@uw.edu>
2026-05-19 12:15:37 -0700

    Reduce spin speed

--- src/assistfour/assistfour/constants.py
+++ src/assistfour/assistfour/constants.py
@@ -11,8 +11,8 @@ COLUMN_6_FRAME = "column_6"
 COLUMN_7_FRAME = "column_7"
 
 # Rates.
-SEARCH_SPIN_RATE = 1.0  # rad / sec; must be <= Pi
-MARKER_SEARCH_PERIOD = 0.5  # seconds (2 Hz); must be >= 2 Hz (<= 0.5 sec)
+SEARCH_SPIN_RATE = 0.5  # rad / sec; must be <= Pi
+MARKER_SEARCH_PERIOD = 0.5  # seconds (2 Hz); must be >= 2 Hz (<= 0.5 seconds)
 MAX_FORWARD_SPEED = 0.5  # m / sec
 MINIMUM_FORWARD_DISTANCE_THRESHOLD = 0.05  # m
 
